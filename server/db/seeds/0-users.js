18f6e3cee13823b098d56ad671fddfdff6823ae3
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-07-22 20:21:03 +1200

    correct table names in users

--- server/db/seeds/0-users.js
+++ server/db/seeds/0-users.js
@@ -1,7 +1,7 @@
 export async function seed(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
+  await knex('users').del()
+  await knex('users').insert([
     { id: 1, username: 'john_doe', email: 'john.doe@example.com', phone: '1234567890' },
     { id: 2, username: 'jane_smith', email: 'jane.smith@example.com', phone: '0987654321' }
   ]);
